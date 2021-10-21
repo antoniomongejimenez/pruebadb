d298c6d9b07dbfbb63927af5b6b49fff1a6912b4
antoniomongejimenez <antonio.monge.jimenez@iesdonana.org>
2021-10-21 18:00:41 +0200

    muestra empleados y departamentos juntos

--- consola.php
+++ consola.php
@@ -3,7 +3,5 @@
 $pdo = new PDO('pgsql:host=localhost;dbname=prueba', 'prueba', 'prueba');
 $sent = $pdo->query('SELECT * FROM depart');
 
-foreach ($sent as $fila) {
-    echo $fila['id']. '|' . $fila['nombre'] . '|' . $fila['localidad']. "\n";
-}
+
 
