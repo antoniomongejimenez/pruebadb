4e1b4573a180c9911d1695f2a1dbec3228e000f2
antoniomongejimenez <antonio.monge.jimenez@iesdonana.org>
2021-10-21 19:19:27 +0200

    Busca por nombre y departamento

--- index.php
+++ index.php
@@ -7,65 +7,65 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    <?php
     require 'auxiliar.php';
+
+    $nombre = (isset($_GET['nombre'])) ? trim($_GET['nombre']) : null;
+    $denominacion = (isset($_GET['denominacion'])) ? trim($_GET['denominacion']) : null;
 
     $pdo = conectar();
 
-
-    $nombre = (isset($_GET['nombre'])) ? trim($_GET['nombre']) : null;
-
-    $sent = $pdo->query("SELECT COUNT(*)
-                           FROM emple e
-                      LEFT JOIN depart d
-                             ON e.depart_id = d.id
-                          WHERE nombre ILIKE '%$nombre%'");
+    $query = "FROM emple e
+         LEFT JOIN depart d
+                ON e.depart_id = d.id
+             WHERE preparar(nombre) LIKE preparar('%$nombre%')
+               AND preparar(denominacion) LIKE preparar('%$denominacion%')";
+    $sent = $pdo->query("SELECT COUNT(*) $query");
     $count = $sent->fetchColumn();
-
-    $sent = $pdo->query("SELECT *
-                           FROM emple e
-                      LEFT JOIN depart d
-                             ON e.depart_id = d.id
-                          WHERE nombre ILIKE '%$nombre%'");
-
+    $sent = $pdo->query("SELECT * $query");
     ?>
+    <div>
+        <form action="" method="GET">
+            <div>
+                <label for="nombre">Nombre: </label>
+                <input id="nombre" type="text" name="nombre"
+                       value="<?= $nombre ?>">
+                <label for="denominacion">Departamento: </label>
+                <input id="denominacion" type="text" name="denominacion"
+                       value="<?= $denominacion ?>">
+            </div>
+            <div>
+                <button type="submit">Filtrar</button>
+            </div>
+        </form>
+    </div>
 
-    
-
-    <form action="">
-        <div>
-            <label for="nombre">Nombre:</label>
-            
-            <input type="text" id="nombre" name="nombre" value = "<?= $nombre ?>">
-        </div>
-    </form>
-
-    <table border="1">
-        <thead>
-            <th>Nombre:</th>
-            <th>Fecha alta:</th>
-            <th>Salario:</th>
-            <th>Departamento:</th>
-            <th>Localidad:</th>
+    <div>
+        <table border="1">
+            <thead>
+                <th>Nombre</th>
+            <th>Fecha de alta</th>
+            <th>Salario</th>
+            <th>Departamento</th>
+            <th>Localidad</th>
         </thead>
         <tbody>
-            <?php foreach ($sent as $fila) {?>
+            <?php foreach ($sent as $fila): ?>
                 <tr>
                     <td><?= $fila['nombre'] ?></td>
                     <td><?= $fila['fecha_alt'] ?></td>
                     <td><?= $fila['salario'] ?></td>
                     <td><?= $fila['denominacion'] ?></td>
                     <td><?= $fila['localidad'] ?></td>
-                </tr>        
-            <?php } ?>
-        </tbody>
-        <tfoot>
-            <td colspan="5">
-                Total de filas:<?= $count ?>
-            </td>
-        </tfoot>
-    </table>
-
-    
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+            <tfoot>
+                <td colspan="5">
+                    Total de filas: <?= $count ?>
+                </td>
+            </tfoot>
+        </table>
+    </div>
 </body>
 </html>
--- prueba.sql
+++ prueba.sql
@@ -1,32 +1,36 @@
 DROP TABLE IF EXISTS depart CASCADE;
 
-create table depart
+CREATE TABLE depart
 (
-    id              bigserial    primary key,
-    denominacion    varchar(255) NOT NULL,
-    localidad       varchar(255)
+    id           bigserial    PRIMARY KEY
+  , denominacion varchar(255) NOT NULL
+  , localidad    varchar(255)
 );
 
 INSERT INTO depart (denominacion, localidad)
-     VALUES ('Contabilidad', 'Sanlucar'),
-            ('Informática', 'Jerez de la Frontera'),
-            ('Ingles', 'Londres'),
-            ('Matematicas', 'Trebujena'),
-            ('Cibernética', 'Chipiona');
-
+    VALUES ('Contabilidad', 'Sanlúcar'),
+           ('Informática', 'Jerez de la Frontera'),
+           ('Inglés', 'Londres'),
+           ('Matemáticas', 'Trebujena'),
+           ('Cibernética', 'Chipiona');
 
 DROP TABLE IF EXISTS emple CASCADE;
 
 CREATE TABLE emple
 (
-    id        bigserial PRIMARY KEY,
-    nombre    varchar(255) NOT NULL,
-    fecha_alt timestamp,
-    salario   numeric(6, 2),
-    depart_id bigint NOT NULL REFERENCES depart (id)
-
+    id        bigserial     PRIMARY KEY
+  , nombre    varchar(255)  NOT NULL
+  , fecha_alt timestamp
+  , salario   numeric(6, 2)
+  , depart_id bigint        NOT NULL REFERENCES depart (id)
 );
 
+CREATE FUNCTION preparar(cadena text) RETURNS text
+LANGUAGE plpgsql as $$
+BEGIN
+    RETURN translate(lower(cadena), 'áéíóú', 'aeiou');
+END;$$;
+
 INSERT INTO emple (nombre, fecha_alt, salario, depart_id)
-     VALUES ('Manolo', '2019-12-04 17:00:00', 2340.75, 2),
-            ('Rosa', '2020-04-05 14:00:00', 2874.99, 4);
+    VALUES ('Manolo', '2019-12-04 17:00:00', 2340.75, 2)
+         , ('Rosa', '2020-04-05 14:00:00', 2874.99, 4);
